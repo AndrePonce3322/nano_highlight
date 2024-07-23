8c1530d7454ba78cecdd8a2561e6374e91081061
Andre Ponce <89555120+AndrePonce3322@users.noreply.github.com>
2024-07-22 18:39:07 -0600

    Already configured added

--- nano_highlight.sh
+++ nano_highlight.sh
@@ -1,11 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-if [ "$EUID" -ne 0 ]
-	then echo "Please run as root"
-	exit
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
 fi
 
 # User
@@ -15,24 +15,36 @@ user=$(getent group sudo | cut -d ':' -f 4)
 cd /usr/share/nano/
 
 # Create file to store the directories
-touch nano_dirsc.txt
+if [ ! -e nano_dirs.txt ]; then
+    touch nano_dirs.txt
+fi
 
 # List all directories
 ls | grep ".nanorc" > nano_dirs.txt
 
-while read line; do
-    copy+="include /usr/share/nano/$line"$"\n"
-done < nano_dirs.txt
-
 root_dir="/root/.nanorc"
 user_dir="/home/$user/.nanorc"
 
-echo -e "$copy" >> $root_dir
-echo -e "$copy" >> $user_dir
+already_configured=false
+
+while read -r line; do
+    include_line="include /usr/share/nano/$line"
+    if grep -qF "$include_line" "$root_dir" && grep -qF "$include_line" "$user_dir"; then
+        already_configured=true
+    else
+        echo "$include_line" >> "$root_dir"
+        echo "$include_line" >> "$user_dir"
+    fi
+done < nano_dirs.txt
+
+if $already_configured; then
+    echo "[*] ALREADY CONFIFURED"
+    exit 1
+fi
 
 echo -e "-------------------------------------------------------------------"
 echo -e "[*] CONTENT COPIED IN $root_dir AND $user_dir: "
-echo -e "[*] LINES COPIED: $(wc nano_dirs.txt | awk '{print $1}')"
+echo -e "[*] LINES COPIED: $(wc -l < nano_dirs.txt)"
 
 echo -e "${BLUE}\n[!] SYNTAX HIGHLIGHTING ENABLED${NC}"
 echo -e "-------------------------------------------------------------------"
